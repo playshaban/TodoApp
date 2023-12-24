444add883b6aff0277355b5f867aedd0e92d800b
playshaban1 <email.playshaban@gmail.com>
2023-12-24 14:36:14 +0530

    cors policies settings

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ route.use(express.json());
 
 route.use(
   cors({
-    origin: ["https://todoapp-puce-alpha.vercel.app","http://127.0.0.1:5500/index.html","https://playshaban.github.io"],
+    origin: ["https://todoapp-puce-alpha.vercel.app","https://playshaban.github.io"],
     methods: ["POST", "GET", "DELETE", "PUT"],
     credentials: true,
   })
